4c2055bf55ca04a5b48ca01f93a6ff51e13aa7b6
Anirban Banerjee <anirbanbanerjee050@gmail.com>
2025-11-07 08:28:41 +0530

    backend update

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -3,17 +3,11 @@ import { Request } from 'express';
 declare global {
   namespace Express {
     interface Request {
-      user?: {
-        id: string;
-        email: string;
-      };
+      user?: any;
     }
   }
 }
 
 export interface AuthenticatedRequest extends Request {
-  user: {
-    id: string;
-    email: string;
-  };
+  user: any;
 }
